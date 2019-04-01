1c009532a1699e2d77a2828acd7464c466723bf0
jungj5 <joe.j.jung@gmail.com>
2019-04-01 18:00:31 -0400

    added api call and iframe list-view functionality

--- client/src/App.tsx
+++ client/src/App.tsx
@@ -1,6 +1,7 @@
 import React, { Component } from 'react';
 import logo from './logo.svg';
 import './App.css';
+import axios from 'axios';
 
 // class App extends Component {
 //   render() {
@@ -25,19 +26,80 @@ import './App.css';
 //   }
 // }
 
-class App extends Component {
+interface response {
+  content: content[]
+}
+
+interface content {
+  type: string,
+  videoId: string,
+  title: string,
+  thumbnailUrl: string,
+  creatorName: string,
+  createdAt: string,
+  embedLink: string,
+  viewCount?: number,
+  redditInfo?: object,
+  videoClipInfo?: object,
+  upvotes?: number
+};
+
+
+// function used to simulate time used for network communication
+function sleep(milliseconds: number) {
+  var start = new Date().getTime();
+  for (var i = 0; i < 1e7; i++) {
+    if ((new Date().getTime() - start) > milliseconds){
+      break;
+    }
+  }
+}
+
+
+class App extends Component<any, response> {
+  constructor(props: any) {
+    super(props);
+
+    this.state = {
+      content: []
+    };
+  }
+
+  async componentDidMount() {
+    try {
+      const res = await axios.get('response1.json');
+      sleep(200);
+      const contentArray = res.data
+      this.setState(contentArray);
+
+    } catch (e) {
+      
+    }
+
+  }
   render() {
+    const results = this.state.content;
     return (
-      <iframe
-        src="https://www.youtube.com/embed/fs5cYAsSM40"
-        height="390"
-        width="640">
-        // frameBorder="0"
-        // scrolling="no">
-        // allowFullScreen="True">
-      </iframe>
+      <ul>
+        {results.map((result: content) =>
+          <li key={result.videoId} >
+            <iframe
+              src={result.embedLink}
+              height="390"
+              width="640"
+              frameBorder="10"
+              scrolling="no">
+              allowFullScreen>
+              >
+            </iframe>
+          </li>
+          )}
+      </ul>
+
     );
   }
 }
 
 export default App;
+
+//https://www.robinwieruch.de/react-fetching-data/
