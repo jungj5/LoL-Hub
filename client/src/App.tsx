390401c17b003caccdd6986ef4bd1a044e67cc42
jungj5 <joe.j.jung@gmail.com>
2019-04-12 17:45:23 -0400

    Initial commit for content filtering

--- client/src/App.tsx
+++ client/src/App.tsx
@@ -2,6 +2,8 @@ import React, { Component } from 'react';
 import logo from './logo.svg';
 import './App.css';
 import axios from 'axios';
+import { render } from 'react-dom';
+import { content } from './stubs/response1.json';
 
 // class App extends Component {
 //   render() {
@@ -26,11 +28,14 @@ import axios from 'axios';
 //   }
 // }
 
-interface response {
-  content: content[]
+interface ComponentState {
+  initialContent: Content[],
+  content: Content[],
+  selectedContentType?: string,
+  selectedRole?: string
 }
 
-interface content {
+interface Content {
   type: string,
   videoId: string,
   title: string,
@@ -44,58 +49,120 @@ interface content {
   upvotes?: number
 };
 
+let streamers = new Map();
+streamers.set('jungle', ['Nightblue3', 'IWDominate']);
+streamers.set('adc', 'Doublelift');
 
-// function used to simulate time used for network communication
-function sleep(milliseconds: number) {
-  var start = new Date().getTime();
-  for (var i = 0; i < 1e7; i++) {
-    if ((new Date().getTime() - start) > milliseconds){
-      break;
-    }
-  }
-}
-
-
-class App extends Component<any, response> {
-  constructor(props: any) {
+class App extends Component<{}, ComponentState> {
+  constructor(props: {}) {
     super(props);
 
     this.state = {
-      content: []
+      initialContent: [],
+      content: [],
+      selectedContentType: 'all',
+      selectedRole: 'all'
     };
   }
 
-  async componentDidMount() {
+  componentDidMount = async () => {
     try {
-      const res = await axios.get('response1.json');
-      sleep(200);
-      const contentArray = res.data
-      this.setState(contentArray);
+      // const res = await axios.get('response1.json');
+      // const contentArray = res.data
+      //this.setState(contentArray);
+      setTimeout(() => {
+        this.setState({
+          initialContent: content, 
+          content: content
+        })
+      }, 200);
 
     } catch (e) {
-      
+      console.log(e);
     }
-
   }
-  render() {
-    const results = this.state.content;
-    return (
-      <ul>
-        {results.map((result: content) =>
-          <li key={result.videoId} >
-            <iframe
-              src={result.embedLink}
-              height="390"
-              width="640"
-              frameBorder="10"
-              scrolling="no">
-              allowFullScreen>
-              >
-            </iframe>
-          </li>
-          )}
-      </ul>
 
+  contentFilter = async (field: string, event: any) => {
+    await this.setState({[field]: event.target.value} as ComponentState);
+    let initialContentList = this.state.initialContent;
+    let selectedContentType = this.state.selectedContentType;
+    let selectedRole = this.state.selectedRole;
+
+    if (selectedContentType === 'all' && selectedRole === 'all') {
+      this.setState({content: this.state.initialContent});
+    } else if (selectedContentType === 'all' && selectedRole !== 'all') {
+      const updatedList = initialContentList.filter((media) => {
+        return streamers.get(selectedRole).includes(media.creatorName);
+      });
+      this.setState({
+        content: updatedList
+      });
+    } else if (selectedContentType !== 'all' && selectedRole === 'all') {
+      const updatedList = initialContentList.filter((media) => {
+        return media.type === selectedContentType;
+      });
+      this.setState({
+        content: updatedList
+      });
+    } else {
+      const updatedList = initialContentList.filter((media) => {
+        return media.type === selectedContentType && streamers.get(selectedRole).includes(media.creatorName);
+      });
+      this.setState({
+        content: updatedList
+      });
+    }
+  }
+
+  render = () => {
+    const results = this.state.content;
+    return results ?
+      this.renderResults(results) :
+      this.renderLoading()
+  }
+
+  renderResults = (results: Content[]) => {
+    return (
+      <div>
+        <select name="selectedContentType" onChange={(event) => this.contentFilter('selectedContentType', event)}>
+          <option value="all">All</option>
+          <option value="youtube-video">Youtube Video</option>
+          <option value="twitch-clip">Twitch Clip</option>
+        </select>
+        <select name="selectedRole" onChange={(event) => this.contentFilter('selectedRole', event)}>
+          <option value ="all">All</option>
+          <option value="jungle">jungle</option>
+          <option value="adc">adc</option>
+        </select>        
+        <ul id="content_unordered_list">
+          {results.map((result: Content) =>
+            <li key={result.videoId} >
+              <iframe
+                src={result.embedLink}
+                height="390"
+                width="640"
+                frameBorder="10"
+                scrolling="no">
+                allowFullScreen>
+              </iframe>
+              <h3>{result.title}</h3>
+              <ul id="content_unordered_list">
+                <li>Creator: {result.creatorName}</li>
+                <li>Content Type: {result.type.replace('-',' ')}</li>
+                <li>Creation Date: {result.createdAt.split(':')[0]}</li>
+              </ul>                
+            </li>
+            )}
+        </ul>
+      </div>
+    );
+  }
+
+  renderLoading = () => {
+    return (
+      <div style={{backgroundColor: "#141414"}}>
+      <img src={logo} className="App-logo" alt="logo" />
+      </div>
     );
   }
 }
@@ -103,3 +170,5 @@ class App extends Component<any, response> {
 export default App;
 
 //https://www.robinwieruch.de/react-fetching-data/
+//https://codepen.io/pjmtokyo/pen/ZGVjVV/
+//https://moduscreate.com/blog/ext-js-to-react-load-sort-and-filter-data-with-react/
