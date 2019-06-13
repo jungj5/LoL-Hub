550b70cd7dd85935b072b10aac1c641c6dc18b13
jungj5 <joe.j.jung@gmail.com>
2019-06-13 19:22:13 -0400

    Updates

---
+++ precache-manifest.f0ff12de74175f4640a342f96080336f.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = [
+  {
+    "revision": "f2e2bdd4950f438776ab",
+    "url": "/lol-hub/static/css/main.58e43d64.chunk.css"
+  },
+  {
+    "revision": "f2e2bdd4950f438776ab",
+    "url": "/lol-hub/static/js/main.b7a33f81.chunk.js"
+  },
+  {
+    "revision": "209ac61905b68619b87e",
+    "url": "/lol-hub/static/js/runtime~main.3732b222.js"
+  },
+  {
+    "revision": "03d367e9dc5f76c9f699",
+    "url": "/lol-hub/static/js/2.50959196.chunk.js"
+  },
+  {
+    "revision": "5d5d9eefa31e5e13a6610d9fa7a283bb",
+    "url": "/lol-hub/static/media/logo.5d5d9eef.svg"
+  },
+  {
+    "revision": "bc361c684e5a53f9850541c50ca4515f",
+    "url": "/lol-hub/index.html"
+  }
+];
